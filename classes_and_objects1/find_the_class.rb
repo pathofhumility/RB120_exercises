f726e3203ac76cb7c56fa5aba667effbb94c509e
Andy Holmes <beawareherenow@gmail.com>
2020-09-03 12:01:45 -0400

    initial

---
+++ classes_and_objects1/find_the_class.rb
@@ -0,0 +1,7 @@
+# find_the_class.rb
+# Update the following code so that, instead of printing the values, each statement prints
+# the name of the class to which it belongs.
+
+puts "Hello".class    # => String
+puts 5.class          # => Integer
+puts [1, 2, 3].class  # => Array
