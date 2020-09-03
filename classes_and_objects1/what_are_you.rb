f45a20e7b38ca6d5550d50ed382e98a39e245306
Andy Holmes <beawareherenow@gmail.com>
2020-09-03 12:38:10 -0400

    initial

---
+++ classes_and_objects1/what_are_you.rb
@@ -0,0 +1,11 @@
+# what_are_you.rb
+# Using the code from the previous exercise, add an `#initialize` method that prints
+# `I'm a cat!` when a new `Cat` object is initialized.
+
+class Cat
+  def initialize
+    puts "I'm a cat!"
+  end
+end
+
+kitty = Cat.new
