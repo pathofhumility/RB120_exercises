42bfa9a6ad8527c252f97b42b06a5768e004fee0
Andy Holmes <beawareherenow@gmail.com>
2020-09-03 12:10:27 -0400

    initial

---
+++ classes_and_objects1/create_the_class.rb
@@ -0,0 +1,12 @@
+# create_the_class.rb
+# Create an empty class named `Cat`.
+
+class Cat
+end
+
+# Class creation is similar to defining a method, but rather than using the
+# reserved word `def`, we use `class`.
+# Also, the class name uses the `CamelCase` format rather than the `snake_case`
+# format used with methods.
+# Formatting in a manner consistant with accepted standards is good practice,
+# ignoring best practices will not cause a logic or runtime error.
