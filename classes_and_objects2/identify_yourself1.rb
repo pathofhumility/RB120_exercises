692757d0bb19da53e1b6301f136f8e00b431d479
Andy Holmes <beawareherenow@gmail.com>
2020-09-04 10:04:14 -0400

    initial

---
+++ classes_and_objects2/identify_yourself1.rb
@@ -0,0 +1,21 @@
+# identify_yourself1.rb
+#
+# Using the following code, add a method named `#identify` that returns
+# its calling object.
+
+class Cat
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def identify
+    self
+  end
+end
+
+kitty = Cat.new('Doe')
+p kitty.identify
+
+# `self` refers to the calling object.
