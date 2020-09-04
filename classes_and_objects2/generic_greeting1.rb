c212ef2a34ad72d341b4d845bb1c16c7b35a7ae5
Andy Holmes <beawareherenow@gmail.com>
2020-09-03 20:20:50 -0400

    initial

--- classes_and_objects2/generic_greeting1.rb
+++ classes_and_objects2/generic_greeting1.rb
@@ -15,3 +15,8 @@ Cat.generic_greeting
 # 1. We are being asked to "do something" when `generic_greeting` is called
 #    on `Cat`. `Cat` is a class, not and instance/object. This informs us
 #    we will need to define a class method rather than an instance method.
+#
+# 2. In order to define a class method we need to define the method on the
+#    class. We can do this by using `self` prepended to the method name or
+#    we could prepend the class name: `Cat.generic_greeting`. The former
+#    is the preferred syntax.
