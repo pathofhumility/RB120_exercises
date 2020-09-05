78d050f71e2dda954b683b15526827721e2550a6
Andy Holmes <beawareherenow@gmail.com>
2020-09-05 15:14:00 -0400

    initial

---
+++ classes_and_objects2/public_secret.rb
@@ -0,0 +1,13 @@
+# public_sectet.rb
+#
+# Using the following code, create a class named `Person` with an instance
+# variable named `@secret`. Use a setter method to add a value to `@sectet`,
+# then use a getter method to print `@secter`.
+
+class Person
+  attr_accessor :secret
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+puts person1.secret
