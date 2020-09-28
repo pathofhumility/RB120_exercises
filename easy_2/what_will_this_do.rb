3f79f06fb7b68def7044eae0c0c2ce3748716f72
Andy Holmes <beawareherenow@gmail.com>
2020-09-28 10:55:04 -0400

    initial

---
+++ easy_2/what_will_this_do.rb
@@ -0,0 +1,20 @@
+# what_will_this_do.rb
+# What will the following code print?
+
+class Something
+  def initialize
+    @data = 'Hello'
+  end
+
+  def dupdata
+    @data + @data
+  end
+
+  def self.dupdata
+    'ByeBye'
+  end
+end
+
+thing = Something.new
+puts Something.dupdata  # => ByeBye
+puts thing.dupdata      # => HelloHello
