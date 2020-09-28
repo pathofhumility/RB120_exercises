b3121677658a654d0b07284eda0c8c20d4880759
Andy Holmes <beawareherenow@gmail.com>
2020-09-28 11:06:25 -0400

    initial

---
+++ easy_2/comparing_wallets.rb
@@ -0,0 +1,31 @@
+# comparing_wallets.rb
+# Consider the given code. Modify the code so that it works.
+# Do not allow the amount in the wallet accessible to any method that isn't part
+# of the Wallet class.
+# Solution: Add the getter method `amount` as a protected method.
+
+class Wallet
+  include Comparable
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def <=>(other_wallet)
+    amount <=> other_wallet.amount
+  end
+
+  protected
+
+  attr_reader :amount
+end
+
+bills_wallet = Wallet.new(500)
+pennys_wallet = Wallet.new(465)
+if bills_wallet > pennys_wallet
+  puts 'Bill has more money than Penny'
+elsif bills_wallet < pennys_wallet
+  puts 'Penny has more money than Bill'
+else
+  puts 'Bill and Penny have the same amount of money.'
+end
