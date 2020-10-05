dd6960fde554903a530b90b8ef033ca19cf61e4e
Andy Holmes <beawareherenow@gmail.com>
2020-10-04 20:31:49 -0400

    initial

---
+++ easy_2/moving.rb
@@ -0,0 +1,65 @@
+# moving.rb
+# Given the following code:
+
+module Walkable
+  def walk
+    puts "#{name} #{gait} forward"
+  end
+end
+
+class Person
+  include Walkable
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "strolls"
+  end
+end
+
+class Cat
+  include Walkable
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "saunters"
+  end
+end
+
+class Cheetah
+  include Walkable
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  private
+
+  def gait
+    "runs"
+  end
+end
+
+mike = Person.new("Mike")
+mike.walk
+
+kitty = Cat.new("Kitty")
+kitty.walk
+
+flash = Cheetah.new("Flash")
+flash.walk
