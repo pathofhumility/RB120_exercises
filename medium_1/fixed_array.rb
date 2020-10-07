460bab6ef355502f17009b2e52bbe4ddeb0f52b8
Andy Holmes <beawarehernow@gmail.com>
2020-10-07 19:52:17 -0400

    add [], to_a methods

--- medium_1/fixed_array.rb
+++ medium_1/fixed_array.rb
@@ -2,6 +2,22 @@
 # Write a class tha implements a fixed-length array, and provides the necessary
 # methods to support the following code:
 
+class FixedArray
+  attr_reader :array
+
+  def initialize(arr_size)
+    @array = Array.new(5)
+  end
+
+  def [](index)
+    array[index]
+  end
+
+  def to_a
+    array
+  end
+end
+
 fixed_array = FixedArray.new(5)
 puts fixed_array[3] == nil
 puts fixed_array.to_a == [nil] * 5
