44d7286f930f71e14e0e454a2b023881130efbbb
Andy Holmes <beawarehernow@gmail.com>
2020-10-09 22:05:51 -0400

    initial

--- medium_1/fixed_array.rb
+++ medium_1/fixed_array.rb
@@ -9,6 +9,11 @@ class FixedArray
     @array = Array.new(5)
   end
 
+  # >> setter method <<
+  def []=(index, element)
+    self.array[index] = element
+  end
+
   def [](index)
     array[index]
   end
@@ -16,10 +21,53 @@ class FixedArray
   def to_a
     array
   end
+
+  def to_s
+    "#{array}"
+  end
 end
 
 fixed_array = FixedArray.new(5)
 puts fixed_array[3] == nil
 puts fixed_array.to_a == [nil] * 5
 
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
 
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
