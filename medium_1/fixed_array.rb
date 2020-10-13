06abab9d7ab6175d22cfd9a88f549179a9a5e7cb
Andy Holmes <beawareherenow@gmail.com>
2020-10-13 17:47:35 -0400

    remove corrupt lines

--- medium_1/fixed_array.rb
+++ medium_1/fixed_array.rb
@@ -1,33 +1,6 @@
-<<<<<<< HEAD
 # fixed_array.rb
-# A fixed-length array is an array that always has a ficed number of elements.
-# Write a class that implements a fixed-lengthed array, and provides the necessary
-# methods to support the following code:
-
-class FixedArray
-  attr_accessor :arr
-
-  def initialize(len)
-    @arr = Array.new(len)
-  end
-
-  def []=(i, element)
-    arr[i] = element
-  end
-
-  def [](i)
-    arr[i]
-  end
-
-  def to_a
-    arr
-  end
-
-  def to_s
-    "#{arr}"
-=======
 # A fixed-length array is an array that always has a fixed number of elements.
-# Write a class tha implements a fixed-length array, and provides the necessary
+# Write a class that implements a fixed-lengthed array, and provides the necessary
 # methods to support the following code:
 
 class FixedArray
@@ -48,7 +21,6 @@ class FixedArray
 
   def to_a
     array
->>>>>>> 460bab6ef355502f17009b2e52bbe4ddeb0f52b8
   end
 
   def to_s
@@ -60,10 +32,6 @@ fixed_array = FixedArray.new(5)
 puts fixed_array[3] == nil
 puts fixed_array.to_a == [nil] * 5
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0c86d935ee7a0a2fde5f3672821bb72b976c7d07
 fixed_array[3] = 'a'
 puts fixed_array[3] == 'a'
 puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
@@ -80,7 +48,6 @@ fixed_array[4] = 'd'
 puts fixed_array[4] == 'd'
 puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
 puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
-<<<<<<< HEAD
 
 puts fixed_array[-1] == 'd'
 puts fixed_array[-4] == 'c'
@@ -105,32 +72,3 @@ begin
 rescue IndexError
   puts true
 end
-=======
-
-puts fixed_array[-1] == 'd'
-puts fixed_array[-4] == 'c'
-
-begin
-  fixed_array[6]
-  puts false
-rescue IndexError
-  puts true
-end
-
-begin
-  fixed_array[-7] = 3
-  puts false
-rescue IndexError
-  puts true
-end
-
-begin
-  fixed_array[7] = 3
-  puts false
-rescue IndexError
-  puts true
-end
-=======
-
->>>>>>> 460bab6ef355502f17009b2e52bbe4ddeb0f52b8
->>>>>>> 0c86d935ee7a0a2fde5f3672821bb72b976c7d07
